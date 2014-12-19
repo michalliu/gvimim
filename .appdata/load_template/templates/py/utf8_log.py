c4bb0dc9aa56c0a632b6951c2d45b7048c88a4ec
michalliu <michalliu@foxmail.com>
2014-12-19 13:19:48 +0800

    add py template with log

---
+++ .appdata/load_template/templates/py/utf8_log.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#coding:utf-8
+# vim: ts=4 sw=4 et
+import sys
+import logging
+
+# Run as module
+logger_name="mylogger"
+logger = logging.getLogger(logger_name)
+
+default_encoding = 'utf-8'
+
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+def init_logger():
+    # run as main
+    global logger
+    f_handler = logging.FileHandler("./%s.txt" % logger_name)
+    f_formatter = logging.Formatter('[%(asctime)s,%(levelname)s,%(filename)s,%(lineno)d]:%(message)s')
+    f_handler.setFormatter(f_formatter)
+    logger.addHandler(f_handler)
+    #console_log
+    c_handler = logging.StreamHandler(sys.stdout)
+    c_formatter = logging.Formatter('[%(levelname)s,%(filename)s,%(lineno)d]:%(message)s')
+    c_handler.setFormatter(c_formatter)
+    logger.addHandler(c_handler)
+    logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger(logger_name)
+
+def main():
+    init_logger()
+    logger.info("hello world")
+    pass
+
+if __name__ == "__main__":
+    main()
